62259498958b35d8720a2ec514ba0d1cb9ca66a8
Yakovlev <3310091@mail.ru>
2022-07-23 13:31:45 +0700

    Init

--- project/dao/main.py
+++ project/dao/main.py
@@ -1,6 +1,22 @@
 from project.dao.base import BaseDAO
-from project.models import Genre
+from project.models import *
 
 
 class GenresDAO(BaseDAO[Genre]):
     __model__ = Genre
+
+
+class DirectorsDAO(BaseDAO[Director]):
+    __model__ = Director
+
+
+class MovieDAO(BaseDAO[Movie]):
+    __model__ = Movie
+
+
+class UserDAO(BaseDAO[User]):
+    __model__ = User
+
+
+
+
--- project/models.py
+++ project/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, Integer, Float, ForeignKey
 
 from project.setup.db import models
 
@@ -7,3 +7,33 @@ class Genre(models.Base):
     __tablename__ = 'genres'
 
     name = Column(String(100), unique=True, nullable=False)
+
+class Director(models.Base):
+    __tablename__ = 'directors'
+
+    name = Column(String(100), unique=True, nullable=False)
+
+class Movie(models.Base):
+    __tablename__ = 'movies'
+
+    title = Column(String(100), unique=False, nullable=False)
+    description = Column(String(100), unique=False, nullable=False)
+    trailer = Column(String(100), unique=False, nullable=False)
+    year = Column(Integer, nullable=False)
+    rating = Column(Float, nullable=False)
+    genre_id = Column(Integer, ForeignKey(f'{Genre.__tablename__}.id'), nullable=False)
+    director_id =  Column(Integer, ForeignKey(f'{Director.__tablename__}.id'), nullable=False)
+
+class User(models.Base):
+    __tablename__ = 'users'
+
+    email = Column(String(100), unique=True, nullable=False)
+    password = Column(String(200), unique=True, nullable=False)
+    name = Column(String(100), unique=True, nullable=False)
+    surname = Column(String(100), unique=True, nullable=False)
+    favourite_genre = Column(Integer, ForeignKey(f'{Genre.__tablename__}.id'), nullable=False)
+
+
+
+
+
